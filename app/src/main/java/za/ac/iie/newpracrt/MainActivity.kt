9758da2ff6a59a69d7e0659a6a40b47eca9bf02e
ST10494318 <ST10494318@rcconnect.edu.za>
2025-06-18 09:30:43 +0000

    button edit

--- app/src/main/java/za/ac/iie/newpracrt/MainActivity.kt
+++ app/src/main/java/za/ac/iie/newpracrt/MainActivity.kt
@@ -1,11 +1,22 @@
 package za.ac.iie.newpracrt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
